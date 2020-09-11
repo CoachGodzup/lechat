0e6069616d26eefe4e82d795eb65b81f7d85761b
Matteo Garza <matteo.garza@facile.it>
2020-09-11 17:38:23 +0200

    Cookie service

---
+++ lechat-fe/src/services/cookie.ts
@@ -0,0 +1,38 @@
+const DEFAULT_COOKIE_EXPIRATION_DATE = 1 * 24 * 60 * 60 * 1000;
+
+export interface Cookie {
+  name: string;
+  value: any;
+  expirationDate?: Date;
+  path?: string;
+}
+
+const DEFAULT_COOKIE: Cookie = {
+  name: '',
+  value: '',
+  expirationDate: new Date(new Date().getTime() + DEFAULT_COOKIE_EXPIRATION_DATE),
+  path: '/'
+}
+
+class CookieService {
+
+  // ma sono impazzito? i cookie? ma usa lo storage!!!
+
+
+  setCookie(newCookie: Cookie): void {
+    debugger;
+    const newCookieWithDefault = { ...DEFAULT_COOKIE, ...newCookie };
+    localStorage.setItem(newCookieWithDefault.name, newCookieWithDefault.value)
+  }
+
+  getCookieByName(name: string): string|null {
+    return localStorage.getItem(name)
+  }
+
+  deleteCookie(name: string): void {
+    localStorage.removeItem(name)
+  }
+}
+
+const cookieService = new CookieService();
+export default cookieService;
